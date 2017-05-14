f308fb2b65f6f9f9e6d535aedce9884f0efd832a
ihalilovic1 <ihalilovic1@etf.unsa.ba>
2017-05-14 18:23:57 +0200

    Responsive Login Page

--- Projekat/FootballSimulator/FootballSimulator/CustomKontrole/AddNewUser.xaml.cs
+++ Projekat/FootballSimulator/FootballSimulator/CustomKontrole/AddNewUser.xaml.cs
@@ -13,24 +13,15 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
-namespace FootballSimulator.View
+namespace FootballSimulator.CustomKontrole
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class Login : Page
+    public sealed partial class AddNewUser : UserControl
     {
-        public Login()
+        public AddNewUser()
         {
             this.InitializeComponent();
-        }
-
-        private void okBtn_Click(object sender, RoutedEventArgs e)
-        {
-            //Take selected user
-            this.Frame.Navigate(typeof(MenuMain), null);
         }
     }
 }
